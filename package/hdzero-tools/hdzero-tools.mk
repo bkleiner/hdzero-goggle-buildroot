3d4a54221e18a719ee8499d61e3f30d46879bf21
bkleiner <b.kleiner@str8labs.com>
2023-01-13 00:40:04 +0100

    add dragonsecboot to hdzero-tools

--- package/hdzero-tools/hdzero-tools.mk
+++ package/hdzero-tools/hdzero-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HDZERO_TOOLS_VERSION = 8506952654984fb11e6ab28ac19861df7c85f34e
+HDZERO_TOOLS_VERSION = c05aa7b8e85666a812cbf6db2f62716a09238777
 HDZERO_TOOLS_SITE = $(call github,bkleiner,hdzero-goggle-tools,$(HDZERO_TOOLS_VERSION))
 HDZERO_TOOLS_INSTALL_STAGING = YES
 
@@ -17,6 +17,7 @@ define HOST_HDZERO_TOOLS_INSTALL_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/parser_mbr $(HOST_DIR)/bin/hdz-parser_mbr
 	$(INSTALL) -D -m 0755 $(@D)/bin/script $(HOST_DIR)/bin/hdz-script
 	$(INSTALL) -D -m 0755 $(@D)/bin/u_boot_env_gen $(HOST_DIR)/bin/hdz-u_boot_env_gen
+	$(INSTALL) -D -m 0755 $(@D)/bin/dragonsecboot $(HOST_DIR)/bin/hdz-dragonsecboot
 endef
 
 $(eval $(host-generic-package))
