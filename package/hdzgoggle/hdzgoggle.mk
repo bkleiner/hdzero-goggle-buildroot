3afce18f6b3af9aee7a878404453343109a32040
bkleiner <b.kleiner@str8labs.com>
2023-01-12 22:25:48 +0100

    fetch prebuilt libs from hdzgoggle

--- package/hdzgoggle/hdzgoggle.mk
+++ package/hdzgoggle/hdzgoggle.mk
@@ -4,17 +4,22 @@
 #
 ################################################################################
 
-HDZGOGGLE_VERSION = toolchain-rebase
+HDZGOGGLE_VERSION = softwinner-install-target
 HDZGOGGLE_SITE = $(call github,bkleiner,hdzero-goggle,$(HDZGOGGLE_VERSION))
 HDZGOGGLE_INSTALL_STAGING = NO
 HDZGOGGLE_INSTALL_TARGET = YES
-HDZGOGGLE_CONF_OPTS = -DUSE_SYSTEM_ZLIB=ON -DUSE_SYSTEM_ALSA_LIB=ON -DUSE_SYSTEM_GLOG=ON
-HDZGOGGLE_DEPENDENCIES = host-pkgconf zlib alsa-lib glog
+HDZGOGGLE_DEPENDENCIES = host-pkgconf
 HDZGOGGLE_SUPPORTS_IN_SOURCE_BUILD = NO 
 HDZGOGGLE_INSTALL_IMAGES = YES
 
+define HDZGOGGLE_EXTRACT_MEDIA
+	cat $(@D)/media.tar.gzaa $(@D)/media.tar.gzab | tar xz -C$(@D)
+endef
+
+HDZGOGGLE_PRE_CONFIGURE_HOOKS += HDZGOGGLE_EXTRACT_MEDIA
+
 define HDZGOGGLE_INSTALL_IMAGES_CMDS
-	cp $(@D)/mkapp/image/app.fex $(BINARIES_DIR)/
+	mksquashfs $(@D)/mkapp/app $(BINARIES_DIR)/app.fex -noappend -all-root
 endef
 
 $(eval $(cmake-package))
