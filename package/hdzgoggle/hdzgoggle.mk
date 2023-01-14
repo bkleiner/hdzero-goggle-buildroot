610fb2ef369c1b428a3d3388aea7d99660570db3
bkleiner <b.kleiner@str8labs.com>
2023-01-14 12:06:03 +0100

    add AMIXER

--- package/hdzgoggle/hdzgoggle.mk
+++ package/hdzgoggle/hdzgoggle.mk
@@ -12,12 +12,6 @@ HDZGOGGLE_DEPENDENCIES = host-pkgconf
 HDZGOGGLE_SUPPORTS_IN_SOURCE_BUILD = NO 
 HDZGOGGLE_INSTALL_IMAGES = YES
 
-define HDZGOGGLE_EXTRACT_MEDIA
-	cat $(@D)/media.tar.gzaa $(@D)/media.tar.gzab | tar xz -C$(@D)
-endef
-
-HDZGOGGLE_PRE_CONFIGURE_HOOKS += HDZGOGGLE_EXTRACT_MEDIA
-
 define HDZGOGGLE_INSTALL_IMAGES_CMDS
 	mksquashfs $(@D)/mkapp/app $(BINARIES_DIR)/app.fex -noappend -all-root
 endef
