e0f93d6bee29924931104d477afdf1f9511cab63
bkleiner <b.kleiner@str8labs.com>
2023-01-14 04:21:40 +0100

    ssh: allow root login

--- board/hdzgoggle/post-build.sh
+++ board/hdzgoggle/post-build.sh
@@ -2,7 +2,11 @@
 set -ex
 
 BOARD_DIR="$(dirname $0)"
-APP_FSTAB="/dev/mmcblk0p3	/mnt/app	squashfs	ro,defaults	0 0"
 
 mkdir -p $TARGET_DIR/mnt/app
-grep -qxF $APP_FSTAB $TARGET_DIR/etc/fstab || echo $APP_FSTAB >> $TARGET_DIR/etc/fstab
+
+APP_FSTAB="/dev/mmcblk0p3	/mnt/app	squashfs	ro,defaults	0 0"
+grep -q $APP_FSTAB $TARGET_DIR/etc/fstab || echo $APP_FSTAB >> $TARGET_DIR/etc/fstab
+
+SSH_PERMIT_LOGIN="PermitRootLogin yes"
+grep -q $SSH_PERMIT_LOGIN ${TARGET_DIR}/etc/ssh/sshd_config || echo $SSH_PERMIT_LOGIN >> ${TARGET_DIR}/etc/ssh/sshd_config
