fb58f94da81f4b799e7ddccb4a9e8b3bf2153ff1
bkleiner <b.kleiner@str8labs.com>
2023-04-10 14:12:06 +0200

    add rtc rule to mdev

--- board/hdzgoggle_common/post-build.sh
+++ board/hdzgoggle_common/post-build.sh
@@ -5,3 +5,5 @@ BOARD_DIR="$(dirname $0)"
 source $BOARD_DIR/functions.sh
 
 ensure_line "PermitRootLogin yes" "$TARGET_DIR/etc/ssh/sshd_config"
+
+ensure_line "rtc0          root:root       660    >rtc" "$TARGET_DIR/etc/mdev.conf"
