3114f97eadd61b0a8b5bc74565f3bf7e4c2f1b48
bkleiner <b.kleiner@str8labs.com>
2023-04-20 02:45:04 +0200

    flash: add sunxi-image, use to generate flash image

--- board/hdzgoggle_flash/post-image.sh
+++ board/hdzgoggle_flash/post-image.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-FLASH_SIZE=33554432
+APP_SIZE=8388608
+OVERLAY_SIZE=4456448
 
 BOARD_DIR="$(dirname $0)"
 COMMON_BOARD_DIR="$(dirname $0)/../hdzgoggle_common"
@@ -9,80 +10,21 @@ source $COMMON_BOARD_DIR/functions.sh
 
 pushd $BINARIES_DIR
 
-cp -v $BOARD_DIR/image/*.fex .
+cp -v $BOARD_DIR/image/*{.fex,.json} .
 hdz-u_boot_env_gen $BOARD_DIR/env.cfg env.fex
 
-ENV_SIZE=$(align_size "$(stat -c%s env.fex)")
-BOOT_SIZE=$(align_size "$(stat -c%s uImage)")
-ROOTFS_SIZE=$(align_size "$(stat -c%s rootfs.squashfs)")
-APP_SIZE=8388608
-OVERLAY_SIZE=4456448
+cp -v uImage boot.fex
+cp -v rootfs.squashfs rootfs.fex
+cp -v app.jffs2 app.fex
 
-cat > mbr.fex << EOF
-[mbr]
-size = 16
+truncate -s $APP_SIZE app.fex
 
-[partition_start]
+mkdir -p overlay/{upper,work}
+mkfs.jffs2 --eraseblock=65536 --pad=$OVERLAY_SIZE -d overlay/ -o overlay.fex
+rm -rf overlay
 
-[partition]
-    name         = boot
-    size         = $(($BOOT_SIZE / 512))
-    user_type    = 0x8000
-
-[partition]
-    name         = rootfs
-    size         = $(($ROOTFS_SIZE / 512)) 
-    user_type    = 0x8000
-
-[partition]
-    name         = env
-    size         = $(($ENV_SIZE / 512))
-    user_type    = 0x8000
-
-[partition]
-    name         = app
-    size         = $(($APP_SIZE / 512)) 
-    user_type    = 0x8000
-
-[partition]
-    name         = overlay
-    size         = $(($OVERLAY_SIZE / 512)) 
-    user_type    = 0x8000
-EOF
-unix2dos mbr.fex
-hdz-script mbr.fex
-hdz-update_mbr mbr.bin 1 mbr.fex
-
-hdz-dragonsecboot -pack boot_package.cfg
-
-cp $BOARD_DIR/genimage.cfg .
-cat >> genimage.cfg << EOF 
-image flash.img {
-	flash {}
-	flashtype = "nor-64M"
-
-	partition boot0 {
-		image = "boot.img"
-		size = 1048576
-	}
-
-	partition kernel {
-		image = "uImage"
-        size = $BOOT_SIZE
-	}
-
-	partition rootfs {
-		image = "rootfs.squashfs"
-        size = $ROOTFS_SIZE
-	}
-
-	partition env {
-		image = "env.fex"
-		size = $ENV_SIZE
-	}
-}
-EOF
+sunxi-image dlinfo gen dlinfo.json dlinfo.fex
+sunxi-image mbr gen mbr.json mbr.fex
+sunxi-image image gen image.json HDZG_OS.bin
 
 popd
-
-support/scripts/genimage.sh -c $BINARIES_DIR/genimage.cfg
--- package/hdzero-tools/hdzero-tools.mk
+++ package/hdzero-tools/hdzero-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HDZERO_TOOLS_VERSION = c05aa7b8e85666a812cbf6db2f62716a09238777
+HDZERO_TOOLS_VERSION = 66703c7b12278857dad3483e9c9080758dca819d
 HDZERO_TOOLS_SITE = $(call github,bkleiner,hdzero-goggle-tools,$(HDZERO_TOOLS_VERSION))
 HDZERO_TOOLS_INSTALL_STAGING = YES
 
@@ -18,6 +18,7 @@ define HOST_HDZERO_TOOLS_INSTALL_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/script $(HOST_DIR)/bin/hdz-script
 	$(INSTALL) -D -m 0755 $(@D)/bin/u_boot_env_gen $(HOST_DIR)/bin/hdz-u_boot_env_gen
 	$(INSTALL) -D -m 0755 $(@D)/bin/dragonsecboot $(HOST_DIR)/bin/hdz-dragonsecboot
+	$(INSTALL) -D -m 0755 $(@D)/bin/sunxi-image $(HOST_DIR)/bin/sunxi-image
 endef
 
 $(eval $(host-generic-package))
